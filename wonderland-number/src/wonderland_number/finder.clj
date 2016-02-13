87266135a9f44129e7b3b6d0af4e76a98060c191
Manu Campos <manu@invisibles.cc>
2016-02-13 12:14:45 +0100

    Wonderland number kata.
    Resolved with lots of help from Manuel Rivero. Thanks @trikitrok

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,26 @@
 (ns wonderland-number.finder)
 
+(def all-numbers-in-wonderland
+  (range 100000 (inc 999999)))
+
+(defn digits [x]
+  (set (str x)))
+
+(defn same-digits?
+  [x y]
+  (= (digits x)
+     (digits y)))
+
+ (defn wonder-num?
+   [x]
+   (and (= 6 (count (str x) ))
+        (every? (same-digits? x)
+                (map #(* x %) [2 3 4 5 6]))))
+
+(defn find-first [pred coll]
+  (some #(when (pred %) %) coll))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [x (find-first wonder-num? all-numbers-in-wonderland)]
+    (println x)
+    x))
